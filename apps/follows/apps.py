1e9b2dd1755293e77cbd5ffd1d169fa59edbac1f
SabrinaSouzaDev <lynxsabri@gmail.com>
2025-05-07 15:30:56 -0300

    Refactor follows app: implement audit logging for Follow model and remove unused history field

--- apps/follows/apps.py
+++ apps/follows/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class FollowsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.follows'
+    
+    
+    def ready(self):
+        from .models import Follow
+        from auditlog.registry import auditlog
+        auditlog.register(Follow)
--- apps/follows/models.py
+++ apps/follows/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from auditlog.registry import auditlog
-from auditlog.models import AuditlogHistoryField
 
 User = get_user_model()
 
 class Follow(models.Model):
-    history = AuditlogHistoryField()
     follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
     followed = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
