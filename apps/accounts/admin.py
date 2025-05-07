fb78b4002ac995009413afcc40b0709efc9fcd16
SabrinaSouzaDev <lynxsabri@gmail.com>
2025-05-07 15:30:49 -0300

    Add inline follow relationships to User admin: implement FollowersInline and FollowingInline classes

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from apps.follows.models import Follow
 from .models import User
+
+
+class FollowersInline(admin.TabularInline):
+    model = Follow
+    fk_name = 'followed'  # Usuário que está sendo seguido
+    extra = 0
+    verbose_name = "Follower"
+    verbose_name_plural = "Followers"
+
+class FollowingInline(admin.TabularInline):
+    model = Follow
+    fk_name = 'follower'  # Usuário que está seguindo
+    extra = 0
+    verbose_name = "Following"
+    verbose_name_plural = "Following"
+
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
